a0d5233418c6c5889b0235bc9a394daa6448439c
Marius <marius.muntean@live.com>
2019-07-31 22:13:02 +0200

    Restricted the thumb control to the contour of its parent

--- lib/AnalogController/AnalogController.cs
+++ lib/AnalogController/AnalogController.cs
@@ -5,8 +5,9 @@ namespace AnalogController
 {
     public partial class AnalogController : ContentView
     {
+        private const string ThumbToCenterAnimation = "AnimateThumbToCenter";
         private readonly BoxView _thumbBoxView;
-        private Func<double, double, double> _getOffset;
+        private readonly Func<double, double, double> _getOffset;
 
         public AnalogController()
         {
@@ -14,14 +15,14 @@ namespace AnalogController
 
             var rootLayout = new RelativeLayout();
 
-            var backgroundBoxView = new Frame() { BackgroundColor = Color.LightGray };
+            _backgroundBoxView = new Frame() { BackgroundColor = Color.LightGray };
             rootLayout.Children.Add(
-                    backgroundBoxView,
+                    _backgroundBoxView,
                     Constraint.RelativeToParent(p => 0.0),
                     Constraint.RelativeToParent(p => 0.0),
                     Constraint.RelativeToParent(p =>
                     {
-                        backgroundBoxView.CornerRadius = (float)(Math.Min(p.Width, p.Height) / 2.0);
+                        _backgroundBoxView.CornerRadius = (float)(Math.Min(p.Width, p.Height) / 2.0);
                         return p.Width;
                     }
                         ),
@@ -49,22 +50,39 @@ namespace AnalogController
             this.Content = rootLayout;
         }
 
+        protected override void OnParentSet()
+        {
+            base.OnParentSet();
+        }
+
         private double previousX = 0.0;
         private double previousY = 0.0;
+        private readonly Frame _backgroundBoxView;
 
         private void OnThumbDragged(object sender, PanUpdatedEventArgs e)
         {
             switch (e.StatusType)
             {
                 case GestureStatus.Started:
+
+                    this.AbortAnimation(ThumbToCenterAnimation);
+
                     previousX = e.TotalX;
                     previousY = e.TotalY;
                     _thumbBoxView.Scale = 0.85;
                     break;
 
                 case GestureStatus.Running:
-                    _thumbBoxView.TranslationX += _getOffset(e.TotalX, previousX);
-                    _thumbBoxView.TranslationY += _getOffset(e.TotalY, previousY);
+                    var newX = _thumbBoxView.TranslationX + _getOffset(e.TotalX, previousX);
+                    var newY = _thumbBoxView.TranslationY + _getOffset(e.TotalY, previousY);
+
+                    var newRadius = Math.Sqrt(Math.Pow(newX, 2) + Math.Pow(newY, 2));
+                    var newTheta = Math.Atan2(newY, newX);
+
+                    newRadius = Math.Min(_backgroundBoxView.Width / 2.0, newRadius);
+
+                    _thumbBoxView.TranslationX = newRadius * Math.Cos(newTheta);
+                    _thumbBoxView.TranslationY = newRadius * Math.Sin(newTheta);
 
                     previousX = e.TotalX;
                     previousY = e.TotalY;
@@ -72,12 +90,25 @@ namespace AnalogController
 
                 case GestureStatus.Completed:
                     _thumbBoxView.Scale = 1.0;
+                    AnimateThumbToCenter();
                     break;
 
                 case GestureStatus.Canceled:
                     _thumbBoxView.Scale = 1.0;
+                    AnimateThumbToCenter();
                     break;
             }
+        }
+
+        private void AnimateThumbToCenter()
+        {
+            var centerAnimation = new Animation(d =>
+            {
+                _thumbBoxView.TranslationX = d * _thumbBoxView.TranslationX;
+                _thumbBoxView.TranslationY = d * _thumbBoxView.TranslationY;
+            }, 1.0, 0.0, Easing.CubicIn);
+
+            centerAnimation.Commit(this, ThumbToCenterAnimation, length: 150);
         }
     }
 }
