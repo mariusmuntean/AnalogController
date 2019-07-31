0b8916e9d0c38bb4fa7f68e491f81320585e53c0
Marius <marius.muntean@live.com>
2019-07-31 21:36:50 +0200

    Added UWP test project

--- lib/AnalogController/AnalogController.cs
+++ lib/AnalogController/AnalogController.cs
@@ -5,40 +5,42 @@ namespace AnalogController
 {
     public partial class AnalogController : ContentView
     {
-        BoxView thumbBoxView;
+        private readonly BoxView _thumbBoxView;
+        private Func<double, double, double> _getOffset;
 
         public AnalogController()
         {
+            _getOffset = Device.RuntimePlatform == Device.Android ? (Func<double, double, double>)((a, b) => a) : (a, b) => a - b;
 
             var rootLayout = new RelativeLayout();
 
-            var backgroundBoxView = new BoxView() { BackgroundColor = Color.LightGray };
+            var backgroundBoxView = new Frame() { BackgroundColor = Color.LightGray };
             rootLayout.Children.Add(
                     backgroundBoxView,
                     Constraint.RelativeToParent(p => 0.0),
                     Constraint.RelativeToParent(p => 0.0),
                     Constraint.RelativeToParent(p =>
                     {
-                        backgroundBoxView.CornerRadius = Math.Min(p.Width, p.Height) / 2.0;
+                        backgroundBoxView.CornerRadius = (float)(Math.Min(p.Width, p.Height) / 2.0);
                         return p.Width;
                     }
                         ),
                     Constraint.RelativeToParent(p => p.Height)
                 );
 
-            thumbBoxView = new BoxView() { Color = Color.FromHex("#FFB3B3B3") };
+            _thumbBoxView = new BoxView() { Color = Color.FromHex("#FFB3B3B3") };
             var thumbDragGestureRecognizer = new PanGestureRecognizer();
             thumbDragGestureRecognizer.PanUpdated += OnThumbDragged;
-            thumbBoxView.GestureRecognizers.Add(thumbDragGestureRecognizer);
+            _thumbBoxView.GestureRecognizers.Add(thumbDragGestureRecognizer);
             rootLayout.Children.Add(
-                    thumbBoxView,
+                    _thumbBoxView,
                     Constraint.RelativeToParent(p => p.Width / 2.0 - (0.1 * p.Width)),
                     Constraint.RelativeToParent(p => p.Height / 2.0 - (0.1 * p.Height)),
                     Constraint.RelativeToParent(p =>
                     {
                         var desiredWidth = 0.2 * p.Width;
 
-                        thumbBoxView.CornerRadius = desiredWidth / 2.0;
+                        _thumbBoxView.CornerRadius = desiredWidth / 2.0;
                         return desiredWidth;
                     }),
                     Constraint.RelativeToParent(p => 0.2 * p.Height)
@@ -47,8 +49,8 @@ namespace AnalogController
             this.Content = rootLayout;
         }
 
-        double previousX = 0.0;
-        double previousY = 0.0;
+        private double previousX = 0.0;
+        private double previousY = 0.0;
 
         private void OnThumbDragged(object sender, PanUpdatedEventArgs e)
         {
@@ -57,21 +59,23 @@ namespace AnalogController
                 case GestureStatus.Started:
                     previousX = e.TotalX;
                     previousY = e.TotalY;
-                    thumbBoxView.Scale = 0.85;
+                    _thumbBoxView.Scale = 0.85;
                     break;
-                case GestureStatus.Running:
-                    thumbBoxView.TranslationX += e.TotalX - previousX;
-                    thumbBoxView.TranslationY += e.TotalY - previousY;
 
+                case GestureStatus.Running:
+                    _thumbBoxView.TranslationX += _getOffset(e.TotalX, previousX);
+                    _thumbBoxView.TranslationY += _getOffset(e.TotalY, previousY);
 
                     previousX = e.TotalX;
                     previousY = e.TotalY;
                     break;
+
                 case GestureStatus.Completed:
-                    thumbBoxView.Scale = 1.0;
+                    _thumbBoxView.Scale = 1.0;
                     break;
+
                 case GestureStatus.Canceled:
-                    thumbBoxView.Scale = 1.0;
+                    _thumbBoxView.Scale = 1.0;
                     break;
             }
         }
